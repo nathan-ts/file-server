d9f41dd80f3b74ffaf4b4742f7568349a143b660
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-19 23:41:54 +0000

    added more data files

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 // server.js
+"use strict"
+const {promises: {readFile}} = require("fs");
 const net = require('net');
+// const fs = require('fs')
 
 const server = net.createServer();
 
 // add this line after server is created, before listen is called
 server.on('connection', (client) => {
   console.log('New client connected!');
-  client.write('Hello there!');
+  client.write("Hello there! Please enter a query starting with '[SEARCH]' to search for a file by filename.");
 
   client.setEncoding('utf8'); // interpret data as text
+
   client.on('data', (data) => {
     console.log('Message from client: ', data)
+    if (data.substring(0,8) === "[SEARCH]") {
+      readFile(data.substring(8)).then(fileBuffer => {
+        client.write(fileBuffer.toString());
+      }).catch(error => {
+        client.write(error.message);
+        process.exit(1);
+      });
+    }
   });
 });
 
 server.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
+
