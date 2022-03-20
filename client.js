29adfa751dcd4947de4c5bc4393a4579e888136d
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-20 02:20:31 +0000

    completed text transfer from server to client

--- client.js
+++ client.js
@@ -1,6 +1,11 @@
 // client.js
 
 const net = require('net');
+const rl = require('readline');
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
 
 const conn = net.createConnection({ 
   host: 'localhost', // change to IP address of computer or ngrok host if tunneling
@@ -10,9 +15,36 @@ const conn = net.createConnection({
 conn.setEncoding('utf8'); // interpret data as text
 
 conn.on('data', (data) => {
-  console.log('Server says: ', data);
+  console.log('Server: ', data);
+  if (data.slice(0,6) === "ENOENT") {
+    process.exit(1);
+  }
 });
 
 conn.on('connect', () => {
   conn.write('Hello from client!');
 });
+
+let input = ""
+
+stdin.on('data', data => {
+  // console.log("\nreceived", data, input);
+  if (data === '\u0003') {  // CTRL+C
+    process.stdout.write(`\r`);
+    process.exit(1);
+  } 
+  // else if (data.charCodeAt(0) === 127) {  // BACKSPACE
+  //   input = input.slice(0,-1);
+  //   process.stdout.write(`\r${input}`);
+  // } 
+  else if (data === '\u000d') { // ENTER
+    conn.write(input);
+    process.stdout.write(`\n`);
+    input = "";
+  } else {
+    input += data;
+    process.stdout.write(`${data}`);
+  }
+});
+
+
--- server.js
+++ server.js
@@ -9,21 +9,25 @@ const server = net.createServer();
 // add this line after server is created, before listen is called
 server.on('connection', (client) => {
   console.log('New client connected!');
-  client.write("Hello there! Please enter a query starting with '[SEARCH]' to search for a file by filename.");
-
+  client.write("Hello! Please enter a query starting with '[SEARCH]' to search for a file by filename.");
   client.setEncoding('utf8'); // interpret data as text
 
+  
   client.on('data', (data) => {
     console.log('Message from client: ', data)
     if (data.substring(0,8) === "[SEARCH]") {
-      readFile(data.substring(8)).then(fileBuffer => {
-        client.write(fileBuffer.toString());
+      console.log("Search query detected.");
+
+      readFile(`./data/${data.substring(8)}`).then(fileBuffer => { //`./data/$[data.substring(8)}`
+        client.write(`Here are your lyrics: ${fileBuffer.toString()}`);
       }).catch(error => {
         client.write(error.message);
+        console.error(error);
         process.exit(1);
       });
     }
   });
+
 });
 
 server.listen(3000, () => {
